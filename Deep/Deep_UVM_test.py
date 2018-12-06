53e97e6b823a9397945b6f6c84fcc9a23019fde4
marcos.saraiva <marcos97moreira@gmail.com>
2018-12-06 19:56:09 +0100

    mutley

--- Deep/Deep_UVM_test.py
+++ Deep/Deep_UVM_test.py
@@ -9,20 +9,32 @@ import numpy as np
 import Deep_UVM as deep
 import time
 
-start_time = time.time()
-                    
+
+#Paramters of the test             
 top = np.array([1,4,4,1])
-N = 12
+N = 40
+Ntraining = 50000
+print('Amount of NN:'+str(N))
+print('Ntraining:'+str(Ntraining))
+print('Forward difference')
+print('e = 10^-4')
+
+#SGD 
+print('SGD Test')
 first_deep = deep.deep(top,N) 
-
-print(first_deep.evaluate_mean(100000))
-
-''' 
-first_deep.train(10000,0.1)
-
+start_time = time.time()
+first_deep.train(Ntraining,0.1)
 print("SGD time in minutes:" + str((time.time()-start_time)/60))
-
 start_time = time.time()
 print(first_deep.evaluate(65000))
 print("MC time in minutes:" + str((time.time()-start_time)/60))
-'''
+
+#ADAM
+print('ADAM Test')
+first_deep = deep.deep(top,N) 
+start_time = time.time()
+first_deep.train_ADAM(Ntraining)
+print("ADAM time in minutes:" + str((time.time()-start_time)/60))
+start_time = time.time()
+print(first_deep.evaluate(65000))
+print("MC time in minutes:" + str((time.time()-start_time)/60))
